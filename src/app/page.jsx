1a5d908e7f0323dd1f50274a18d074f1b7c6553d
Karolina <--global>
2025-09-26 19:35:52 -0300

    feature: criação da página inicial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,11 @@
+import css from style.css
+
 export default function Home() {
   return (
     <>
+    <h1>Prova Versionamento - RPV Senai Cataguases</h1>
+    <p class = "p1">Karolina de Souza Batista Esteves</p>
+    <p class = "p2">Cataguases, 02 de Julho de 202</p>
     </>
   )
 }
